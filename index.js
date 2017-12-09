6c7faa1d43a153fcb0a777a9b438c98fd2fa4ef4
James Gary <bucket@earthlink.net>
2017-12-09 21:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value)
   var copy = Object.assign ({}, object,{[key]:value})
   return copy
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  object.key=value;
+  return object;
+}
